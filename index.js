5b0c8bea0213467210b5cdb8c8d22fcb33105fa7
shariful-ire <shariful.ire@gmail.com>
2026-07-01 04:11:50 +0600

    done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import app from './src/app.js';
 import connectDB from './src/config/db.js';
 
-connectDB();
-
-export default app;
+export default async function handler(req, res) {
+  await connectDB();
+  return app(req, res);
+}
--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,21 @@
 import mongoose from 'mongoose';
 
+let cached = global._mongoConn ?? (global._mongoConn = { conn: null, promise: null });
+
 export default async function connectDB() {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error('MongoDB connection error:', err.message);
-    process.exit(1);
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGODB_URI).then(m => m);
   }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (err) {
+    cached.promise = null;
+    console.error('MongoDB connection error:', err.message);
+    throw err;
+  }
+
+  return cached.conn;
 }
